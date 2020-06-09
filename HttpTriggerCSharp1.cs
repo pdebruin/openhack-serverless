a66784d44028821c8b7cfb615fcbee712d6b6b56
Pieter de Bruin <pdebruin@hotmail.com>
2020-06-09 20:50:16 +0200

    anonymous

--- HttpTriggerCSharp1.cs
+++ HttpTriggerCSharp1.cs
@@ -14,7 +14,7 @@ namespace BFYOC.ex2
     {
         [FunctionName("HttpTriggerCSharp1")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
